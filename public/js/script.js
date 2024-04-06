8ad082f2948039828e31a141e0066dd5471237a4
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-04-06 12:41:14 +0530

    whole lobby code added to index.ejs and script.js

--- public/js/script.js
+++ public/js/script.js
@@ -12,8 +12,27 @@ let constraints = {
   // audio: true,
 };
 
+const lobbyContainer = document.getElementById("lobby-container");
+const videoContainer = document.getElementById("video-container");
+
 document.addEventListener("DOMContentLoaded", () => {
-  getUserMedia(constraints);
+  let joinButton = document.getElementById("join");
+  const roomId = document.getElementById("join_link");
+
+  joinButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (roomId.value === "") {
+      alert("room cannot be null");
+    }
+    roomId.value = "";
+    lobbyContainer.style = "display: none";
+    videoContainer.style = "display: block";
+
+    socket.emit("join-room", roomId.value);
+
+    getUserMedia(constraints);
+  });
 });
 
 const getUserMedia = async (constraints) => {
